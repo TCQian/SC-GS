5e8e92ac4603f9659110a5d3db495b03b859e2cd
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-04-18 18:37:47 +0800

    update script

--- soc-cluster.sh
+++ soc-cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=SC-GS                  # Job name
+#SBATCH --job-name=SC-GS                 # Job name
 #SBATCH --time=6:00:00                   # Time limit hrs:min:sec
-#SBATCH --gres=gpu:a100-40:1
+#SBATCH --gres=gpu:h100-47:1             # must use this GPU, since pytorch3d relied on it
 #SBATCH --mail-type=ALL                  # Get email for all status updates
 #SBATCH --mail-user=e0407638@u.nus.edu   # Email for notifications
 #SBATCH --mem=16G                        # Request 16GB of memory
