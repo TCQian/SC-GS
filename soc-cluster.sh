ee16944a69d2649bd73fb0da8e0a010c6b34ab10
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-06-07 14:57:47 +0800

    run cmu without blender

--- soc-cluster.sh
+++ soc-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=SC-GS                 # Job name
+#SBATCH --job-name=SC-GS-cmu                 # Job name
 #SBATCH --time=6:00:00                   # Time limit hrs:min:sec
 #SBATCH --gres=gpu:h100-47:1             # must use this GPU, since pytorch3d relied on it
 #SBATCH --mail-type=ALL                  # Get email for all status updates
@@ -10,4 +10,4 @@
 source ~/.bashrc
 conda activate scgs
 
-CUDA_VISIBLE_DEVICES=0 python train_gui.py --source_path ./data/dnerf/jumpingjacks --model_path outputs/jumpingjacks --deform_type node --node_num 512 --hyper_dim 8 --is_blender --eval --gt_alpha_mask_as_scene_mask --local_frame --resolution 2 --W 800 --H 800
+CUDA_VISIBLE_DEVICES=0 python train_gui.py --source_path ./data/cmu/bastketball --model_path outputs/bastketball --deform_type node --node_num 512 --hyper_dim 8 --eval --gt_alpha_mask_as_scene_mask --local_frame --resolution 2 --W 800 --H 800
